a4e616e10d76f8a01cf49fd6f29bd74d0c439095
Nelson Silva <nelson.silva@inevo.pt>
2013-12-11 15:49:10 +0000

    Updated example to use streams and added logging

--- example/echo.dart
+++ example/echo.dart
@@ -1,37 +1,39 @@
 library echo;
 
 import "dart:html";
-import "dart:convert";
+import 'package:logging/logging.dart';
 import "package:sockjs_client/sockjs.dart" as SockJS;
 
 DivElement div  = querySelector('#first div');
 InputElement inp  = querySelector('#first input');
 FormElement form = querySelector('#first form');
 
-print(m, [p = '']) {
-  if(!p.isEmpty) {
-    p = JSON.encode(p);
-  }
+_log(LogRecord l) {
   div
-    ..append(new Element.html("<code/>")..text=("$m$p"))
-    ..append(new Element.html("<br>"));
-
-  div.scrollTop += 10000;
+    ..append(new Element.html("<code/>")..text = "${l.message}")
+    ..append(new Element.html("<br>"))
+    ..scrollTop += 10000;
 }
 
 main() {
-  print("Starting");
+  // Setup Logging
+  Logger.root.level = Level.INFO;
+  Logger.root.onRecord.listen(_log);
+
+  final LOG = new Logger("sockjs");
+
+  LOG.info("Starting");
   var sockjs_url = 'http://127.0.0.1:8081/echo';
   var sockjs = new SockJS.Client(sockjs_url, protocolsWhitelist:['websocket', 'xhr-streaming'], debug: true);
   querySelector('#first input').focus();
 
-  sockjs.on.open.add( (_) => print('[*] open ${sockjs.protocol}') );
-  sockjs.on.message.add( (e) => print('[.] message ${e.data}') );
-  sockjs.on.close.add( (_) => print('[*] close') );
+  sockjs.onOpen.listen( (_) => LOG.info('[*] open ${sockjs.protocol}') );
+  sockjs.onMessage.listen( (e) => LOG.info('[.] message ${e.data}') );
+  sockjs.onClose.listen( (_) => LOG.info('[*] close') );
 
   inp.onKeyUp.listen( (KeyboardEvent e) {
     if (e.keyCode == 13) {
-      print('[ ] sending ${inp.value}');
+      LOG.info('[ ] sending ${inp.value}');
       sockjs.send(inp.value);
       inp.value = '';
     }
